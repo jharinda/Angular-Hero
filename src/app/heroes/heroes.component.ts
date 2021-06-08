7db9fe62c33879dd9569bf1f650b68efd327a707
Janith Harinda <jharinda@gmail.com>
2021-06-08 18:22:42 +0530

    message service showing what heroes user have clicked

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,9 @@
+import { MessageService } from './../message.service';
+import { Component, OnInit } from '@angular/core';
+
 import { HeroService } from './../hero.service';
-import { HEROES } from './../mock-heroes';
 import { Hero } from './../hero';
 
-
-
-import { Component, OnInit } from '@angular/core';
 
 
 @Component({
@@ -13,7 +12,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  constructor(private heroService: HeroService) { }
+  selectedHero?: Hero;
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -24,11 +26,11 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes().subscribe((heroes) => this.heroes = heroes);
   }
 
-  heroes: Hero[] = [];
 
-  selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroComponent: Selected hero id = ${hero.id}`);
+
   }
 
 
